d0bc4cf26922fe1f921b1602851a1018757dcc82
DrGoudan01 <18813290224@163.com>
2017-11-08 09:37:00 +0800

    使用了包括view，url，config，wsgi四个部分，并且设置了从环境变量导入debug设置

--- hello world.py
+++ hello world.py
@@ -10,10 +10,18 @@ urlpatterns = (
 )
 #定义了url的规则
 from django.conf import settings
+import os
+
+DEBUG=os.environ.get('DEBUG','on') == on,
+SECRET_KEY = os.environ.get('SECRET_KEY',os.urandom(32)),
+ALLOWED_HOST=os.environ.get('ALLOWED_HOST','LOCALHOST','localhost').split(','),
 settings.configure(
-    DEBUG=True,
-    SECRET_KEY = 'thisisthesecretkey',
-    ROOT_URLCONF=__name__,
+    #增加debug的设置选项，直接从系统环境变量中读取，使用os库
+    #当debug为off时，需要借助allowed hosts变量的设置
+    DEBUG = DEBUG,
+    SECRET_KEY = SECRET_KEY,
+    ALLOWED_HOST = ALLOWED_HOST,
+    ROOT_URLCONF = __name__,
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
@@ -22,6 +30,10 @@ settings.configure(
 
 )
 #导入设置文件，然后定义一个启动函数
+#需要一个wsgi接口
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
 #其中sys.argv是一个参数导入函数
 import sys
 if __name__ == '__main__':
